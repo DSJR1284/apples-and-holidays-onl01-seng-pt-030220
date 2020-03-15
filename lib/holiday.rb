2d3c5731e2785bf9f67b875e3b4d4d37faf6bbed
Doug Jones <dsjr1284@gmail.com>
2020-03-15 22:13:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,8 +43,10 @@ end
 
 
 def all_winter_holiday_supplies(holiday_hash)
-  
-
+   array = holiday_hash[:winter].map do |holiday, items|
+    items
+  end
+  array.flatten
 end
 
 # def all_supplies_in_holidays(holiday_hash)
