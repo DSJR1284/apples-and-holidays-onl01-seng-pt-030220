1328b951830d3923481b046b543f71aca2516f52
Doug Jones <dsjr1284@gmail.com>
2020-03-15 21:48:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,40 +29,40 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 end
 
 
-def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
+# def add_supply_to_memorial_day(holiday_hash, supply)
+#   # again, holiday_hash is the same as the ones above
+#   # add the second argument to the memorial day array
 
-hoilday_hash[:spring][:memorial_day] << supply 
-end
+# hoilday_hash[:spring][:memorial_day] << supply 
+# end
 
-def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+# def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   
-  holiday_hash[season][holiday_name] = supply_array
-  holiday_hash
-end
+#   holiday_hash[season][holiday_name] = supply_array
+#   holiday_hash
+# end
 
-def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+# def all_winter_holiday_supplies(holiday_hash)
+#   # return an array of all of the supplies that are used in the winter season
 
-end
+# end
 
-def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
+# def all_supplies_in_holidays(holiday_hash)
+#   # iterate through holiday_hash and print items such that your readout resembles:
+#   # Winter:
+#   #   Christmas: Lights, Wreath
+#   #   New Years: Party Hats
+#   # Summer:
+#   #   Fourth Of July: Fireworks, BBQ
+#   # etc.
 
-end
+# end
 
-def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+# def all_holidays_with_bbq(holiday_hash)
+#   # return an array of holiday names (as symbols) where supply lists
+#   # include the string "BBQ"
 
-end
+# end
 
 
 
